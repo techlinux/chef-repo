626c841e16a0c2d3d67c308c43504f055e78ed77
Aman Hanjrah <a.hanjrah.1@gmail.com>
2016-09-30 12:52:08 +0530

    nginx recipe attempt 1

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -2,30 +2,19 @@
 # Cookbook Name:: nginx
 # Recipe:: default
 #
-# Author:: AJ Christensen <aj@junglist.gen.nz>
+# Copyright 2016, techlinux
 #
-# Copyright 2008-2013, Chef Software, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-include_recipe "nginx::#{node['nginx']['install_method']}"
+package 'nginx'
 
 service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action   :start
+  action [ :enable, :start ]
 end
 
-node['nginx']['default']['modules'].each do |ngx_module|
-  include_recipe "nginx::#{ngx_module}"
+template "#{document_root}/index.html" do
+  source 'index.html.erb'
+  mode '0644'
+  variables(
+    :message => node['motd']['message'],
+    :port => node['nginx']['port']
+  )
 end
