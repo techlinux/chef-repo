63b45816fcd18ced6341141371cebffab99ed1b3
Aman Hanjrah <a.hanjrah.1@gmail.com>
2016-10-23 16:03:58 +0530

    23 Oct, 2016

---
+++ cookbooks/ntp/metadata.rb
@@ -0,0 +1,7 @@
+name             'ntp'
+maintainer       'techlinux'
+maintainer_email 'aman.hanjrah@gmail.com'
+license          ''
+description      'Installs/Configures ntp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
